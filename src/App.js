3802114fee508e3719379465b961489e2821c572
daliDeli <bruns8900@gmail.com>
2018-01-16 11:19:29 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 
+import Footer from "./common/Footer";
 import Header from "./common/Header";
 import MainPage from "./components/MainPage";
 import Authors from "./components/Authors";
@@ -21,6 +22,7 @@ class App extends Component {
          <Route path="/authors/:id" component={SingleAuthorsPage}/>
          <Route path="/about" component={About}/>
        </Switch>
+       <Footer/>
       </div>
     );
   }
@@ -28,5 +30,46 @@ class App extends Component {
 
 export default App;
 
+$(document).on('change keyup paste', '#searchMovie', function () {
+  let input = $('#searchMovie').val();
+  console.log(input);
+
+  let request = $.ajax({
+      url: 'http://api.tvmaze.com/search/shows',
+      method: 'GET',
+      data: {
+          q: input
+      }
+  });
+
+  request.done(output => {
+
+      $('.searchList').html('');
+      console.log(output);
+      output.forEach((e, i) => {
+          let title = $('<li>');
+          let link = $('<a>');
+
+          link.text(e.show.name);
+          link.attr({
+              'value': e.show.id,
+              'href': '#'
+          });
+
+          title.append(link);
+          $('.searchList').append(title);
+      })
+  });
+
+
+});
+
+
+$(document).on('click', 'a', function () {
+  var id = $(this).attr('value');
+
+  localStorage.setItem('key', id);
+  location.replace('single.html');
+});
 
 
